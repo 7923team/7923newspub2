51675593688bc9ae2be472090bd795f3de6f97fa
somebodylive <365201378@qq.com>
2021-12-20 14:12:33 +0800

    master

--- src/main/java/news/beans/W_statDAO.java
+++ src/main/java/news/beans/W_statDAO.java
@@ -1,10 +1,16 @@
 package news.beans;
 
+import news.mapper.W_statMapper;
+import news.beans.W_stat;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.*;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.*;
 
 public class W_statDAO {
     /**
@@ -12,30 +18,36 @@ public class W_statDAO {
      * @return
      */
     public ArrayList<W_stat> getAllWord(){
-        W_stat wStat = null;
         ArrayList<W_stat> wStatsList = new ArrayList<W_stat>();
+        try {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader("mybatis-conf.xml"));
+            SqlSession session = sqlSessionFactory.openSession();
+            W_statMapper w_statMapper = session.getMapper(W_statMapper.class);
+            wStatsList = w_statMapper.wstatlist();
 
-        Connection conn = null;
-        ResultSet rs = null;
-        Statement stmt = null;
-        try{
-            conn = DBGet.getConnection();
-            stmt = conn.createStatement();
-            String sql = "select * from w_stat";
-            rs = stmt.executeQuery(sql);
-            while(rs.next()){
-                wStat = new W_stat();
-                wStat.setId(rs.getInt("id"));
-                wStat.setName(rs.getString("name"));
-                wStat.setNum(rs.getString("num"));
-                wStatsList.add(wStat);
-            }
-        }catch(SQLException el){
-            System.out.println(el+"dao");
-        }finally{
-            DBGet.closeConnection(conn);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return wStatsList;
+//        Connection conn = null;
+//        ResultSet rs = null;
+//        Statement stmt = null;
+//        try{
+//            conn = DBGet.getConnection();
+//            stmt = conn.createStatement();
+//            String sql = "select * from w_stat";
+//            rs = stmt.executeQuery(sql);
+//            while(rs.next()){
+//                wStat = new W_stat();
+//                wStat.setId(rs.getInt("id"));
+//                wStat.setName(rs.getString("name"));
+//                wStat.setNum(rs.getString("num"));
+//                wStatsList.add(wStat);
+//            }
+//        }catch(SQLException el){
+//            System.out.println(el+"dao");
+//        }finally{
+//            DBGet.closeConnection(conn);
+//        }
     }
-
 }
---
+++ src/main/java/news/mapper/W_statMapper.java
@@ -0,0 +1,9 @@
+package news.mapper;
+
+import news.beans.W_stat;
+
+import java.util.ArrayList;
+
+public interface W_statMapper {
+    ArrayList<W_stat> wstatlist();
+}
